8d1315015700fdc0b1626a697360dae99f0563d4
DanielaMoraDevJourney <danielamoradev@outlook.com>
2025-07-09 13:27:36 -0500

    Update: Packages and Urls

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import axios from "axios";
 import "../pages/Login.css"; // Archivo CSS para mejorar el diseño
 import "/black_on_white.png"
+
 function Login() {
   const navigate = useNavigate();
   const [formData, setFormData] = useState({ correo: "", contrasena: "" });
@@ -10,6 +11,7 @@ function Login() {
 
   const handleInputChange = (e) => {
     const { id, value } = e.target;
+    console.log(`Input cambiado: ${id} = ${value}`);
     setFormData((prevData) => ({
       ...prevData,
       [id]: value,
@@ -18,25 +20,33 @@ function Login() {
 
   const handleLogin = async (e) => {
     e.preventDefault();
+    console.log("Intentando iniciar sesión con:", formData);
+
     try {
       const response = await axios.post(
         "https://api-getaway-freelancer.azure-api.net/login/api/freelancer/login",
         formData
       );
+      console.log("Respuesta del login:", response);
+      console.log("Token recibido:", response.data.token);
+
       localStorage.setItem("token", response.data.token);
+      console.log("Token guardado en localStorage.");
+
       navigate("/main");
     } catch (err) {
+      console.error("Error durante el login:", err);
+      if (err.response) {
+        console.error("Detalles del error:", err.response.status, err.response.data);
+      }
       setError("Credenciales incorrectas. Inténtalo de nuevo.");
     }
   };
 
   return (
     <div className="login-container">
-      
       <div className="login-card">
-      <img src="/black_on_white.png" alt="Logo" className="login-logo" />
-
-      
+        <img src="/black_on_white.png" alt="Logo" className="login-logo" />
         <h2 className="text-center">Iniciar Sesión</h2>
         {error && <div className="alert alert-danger">{error}</div>}
         <form onSubmit={handleLogin}>
